f59806214da783355317cd6eac98e0fe84c4ae6f
Francho19 <Frankjosemirandabeleno331@gmail.com>
2025-03-01 13:03:20 -0500

    Add files via upload

---
+++ Tiendaropa.java
@@ -0,0 +1,13 @@
+public class Tiendaropa {
+    public static void main(String[] args) {
+    int precioCamiseta= 25;
+    int precioPantalon =30;
+     float descuento= 0,15;
+     Scanner cantidadCamiseta;
+     Scanner cantidadPantalones;
+     float precioCamiseta= precioCamiseta * cantidadCamiseta;
+     float precioPantalon = precioPantalon * cantidadPantalones;
+float decuentoTotal=(precioCamiseta+precioPantalon )*0.15;
+float pagartotal= (precioCamiseta +precioPantalon)- decuentoTotal;
+    }
+}
